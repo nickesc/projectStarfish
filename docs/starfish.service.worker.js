a9fdc1df8612238e2d1760d9f32e39a562c48a3f
N. Escobar <nick@nescobar.media>
2023-04-24 14:44:19 -0400

    update build

--- docs/starfish.service.worker.js
+++ docs/starfish.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1682355934|44013399138";
+const CACHE_VERSION = "1682361818|44348050676";
 const CACHE_PREFIX = "Starfish-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "starfish.offline.html";
